46c1f7237a84822b482f7650d2dc5c5b5b68cfba
Jhoan Sebastian <js.sebas5@gmai.com>
2017-05-31 17:04:25 -0500

    modelo del caso de uso de registro de ingreso de  productos

---
+++ Models/IngresarProducto.php
@@ -0,0 +1,28 @@
+<?php
+include_once("BaseDeDatos.php");
+class IngresarProducto
+{
+    var $producto;
+    var $cantidad;
+
+    public function __construct($producto,$descripcion)
+    {
+        $this->producto=$producto;
+        $this->cantidad=$cantidad;
+    }
+
+    public function  ingresarProducto()
+    {
+        $BDD=new BaseDeDatos();
+        $temp=$BDD->ConectarBDD();
+        $Sql="insert into productos values('$this->producto',$this->cantidad);";
+        $result=pg_exec($Sql);
+        if (!$result){
+            echo ("Error al ingresar el gasto");
+        }
+        pg_exec($Sql1);
+        echo json_encode("Gasto ingresado con exito");
+    }
+}
+
+?>
