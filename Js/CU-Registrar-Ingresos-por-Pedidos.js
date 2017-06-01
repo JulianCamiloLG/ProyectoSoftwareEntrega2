e46f7302a9b1f961d953d4fc970075b0931f21a4
sergiosoftware <sergio.1701311587@ucaldas.edu.co>
2017-05-31 21:35:42 -0500

    ........

---
+++ Js/CU-Registrar-Ingresos-por-Pedidos.js
@@ -0,0 +1,9 @@
+//Ingresar Productos
+$("#IngresarProducto").button().on("click", function(event){
+    producto=$("#idProducto").val()
+    cantidad=$("#txtCantidadProducto").val()
+
+    $.post("../Controller/controladora.php", {producto:producto, cantidad:cantidad}, funcion:10, function(respuesta){
+        alert(respuesta);
+    })
+})
