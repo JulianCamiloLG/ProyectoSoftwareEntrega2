fac58c6653620221924ff6277e46e23d96250fe3
Jhoan Sebastian <js.sebas5@gmai.com>
2017-06-01 17:00:14 -0500

    b

--- Js/CU-Registrar-Ingresos-por-Pedidos.js
+++ Js/CU-Registrar-Ingresos-por-Pedidos.js
@@ -3,7 +3,7 @@ $("#IngresarProducto").button().on("click", function(event){
     producto=$("#idProducto").val()
     cantidad=$("#txtCantidadProducto").val()
 
-    $.post("../Controller/controladora.php", {producto:producto, cantidad:cantidad}, funcion:10, function(respuesta){
+    $.post("../Controller/controladora.php", {producto:producto, cantidad:cantidad, funcion:10}, function(respuesta){
         alert(respuesta);
     })
 })
